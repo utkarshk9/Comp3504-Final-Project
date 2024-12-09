c8d361bd31d06b51ff70190e32513461e8ae26c5
Utkarsh <utkarshkapoor74@yahoo.com>
2024-12-08 17:43:30 -0700

    Update: backend routes for user, admin, and create  payment to impliment stripe

--- Backend/routes/index.js
+++ Backend/routes/index.js
@@ -2,10 +2,14 @@ const userRoutes = require('./userRoutes');
 const eventRoutes = require('./eventRoutes');
 const feeRoutes = require('./feesRoutes');
 const attendeeRoutes = require('./AttendeeRoutes');
+const adminRoutes = require('./adminRoutes');
+const paymentsRoutes = require('./paymentsRoutes');
 
 module.exports.register = (app, database) => {
     userRoutes.register(app, database);
     eventRoutes.register(app, database);
     feeRoutes.register(app, database);
     attendeeRoutes.register(app, database);
+    adminRoutes.register(app, database);
+    paymentsRoutes.register(app, database);
 };
